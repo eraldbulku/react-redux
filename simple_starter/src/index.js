fccd5f9c97f39824f132481ed99e21818ca9b340
Erald Bulku <erald.b@scopicsoftware.com>
2018-08-15 18:32:04 +0200

    react-redux (section 2 1)

--- simple_starter/src/index.js
+++ simple_starter/src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAUd3A0azJUbu8if_AHOgNQQBW0xpupESs';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+    console.log(data);
+});
 
 // Create new component. This component should produce some HTML
 const App = () => {
@@ -15,5 +20,4 @@ const App = () => {
 }
 
 
-// Take this component/s generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
