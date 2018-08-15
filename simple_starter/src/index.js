8b88a455203ddaa333e592e188b00a13a1d91552
Erald Bulku <erald.b@scopicsoftware.com>
2018-08-15 19:00:13 +0200

    react-redux (section 2 22-25)

--- simple_starter/src/index.js
+++ simple_starter/src/index.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from'youtube-api-search';
 
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyAUd3A0azJUbu8if_AHOgNQQBW0xpupESs';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-    console.log(data);
-});
-
 // Create new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
+            this.setState({videos: data});
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 
