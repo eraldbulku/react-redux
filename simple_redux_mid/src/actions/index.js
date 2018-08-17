52a35fbeaca8fdc434e06cd72e0dd63d98d0c036
Erald Bulku <erald.b@scopicsoftware.com>
2018-08-17 14:33:38 +0200

    react-redux (section-5 58)

--- simple_redux_mid/src/actions/index.js
+++ simple_redux_mid/src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '24d1ab3a1d9c12beb1a626edc5f9a101';
-const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
