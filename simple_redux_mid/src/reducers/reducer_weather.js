6fec9327b2ea4e451d06f0815b71c7d71946a516
Erald Bulku <erald.b@scopicsoftware.com>
2018-08-17 15:41:15 +0200

    react-redux (section-5 60)

--- simple_redux_mid/src/reducers/reducer_weather.js
+++ simple_redux_mid/src/reducers/reducer_weather.js
@@ -1,5 +1,14 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
     console.log('Action received:', action);
-    
+
+    switch (action.type) {
+        case FETCH_WEATHER:
+            return state.concat([action.payload.data]);
+            //return [action.payload.dat, ...state]; // [city, city, city]
+            //return state.push(action.payload.data); // WRONG, do not manipulate state
+    }
+
     return state;
 }
