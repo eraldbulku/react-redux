16aa992106eec060e94278486c64992005a3c4eb
Erald Bulku <erald.b@scopicsoftware.com>
2018-08-17 13:42:00 +0200

    react-redux (section-5 54-56)

---
+++ simple_redux_mid/src/actions/index.js
@@ -0,0 +1,1 @@
+const API_KEY = '24d1ab3a1d9c12beb1a626edc5f9a101';
--- simple_redux_mid/src/containers/search_bar.js
+++ simple_redux_mid/src/containers/search_bar.js
@@ -12,15 +12,18 @@ export default class SearchBar extends Component {
         this.setState({term: event.target.value});
     }
 
+    onFormSubmit(event) {
+        event.preventDefault();
+    }
+
     render() {
         return (
-            <form className="input-group">
+            <form onSubmit={this.onFormSubmit} className="input-group" >
                 <input 
                     placeholder="Get a five-day forecast in your favorite cities"
                     className="form-control"
                     value={this.state.term}
-                    onChange={this.onInputChange}
-                />
+                    onChange={this.onInputChange} />
                 <span className="input-group-btn">
                     <button type="submit" className="btn btn-seconday">Submit</button>
                 </span>
