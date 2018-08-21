ec77b37a4e7357954b80785173773d9b3fbbb6f7
Erald Bulku <erald.b@scopicsoftware.com>
2018-08-21 16:55:09 +0200

    react-redux (section-9 145)

--- simple_redux_3/src/components/posts_show.js
+++ simple_redux_3/src/components/posts_show.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from'react-redux';
+import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
+	componentDidMount() {
+		const { id } = this.props.match.params;
+		this.props.fetchPost(id);
+	}
+
 	render() {
+		//posts[this.props.match.props.id]; // the post that we want to show
+		// this.props === ownProps
+		
 		return (
 			<div>
 				Posts Show!
@@ -10,4 +20,8 @@ class PostsShow extends Component {
 	}
 }
 
-export default PostsShow;
+function mapStateToProps({ posts }, ownProps) {
+	return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
